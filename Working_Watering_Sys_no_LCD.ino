2701d7605271116d0d920bfa7fb7137756ab94ca
Bumblebee2019 <57073212+Bumblebee2019@users.noreply.github.com>
2022-05-19 09:28:08 -0700

    Add files via upload

---
+++ Working_Watering_Sys_no_LCD.ino
@@ -0,0 +1,41 @@
+
+//initializing connection between relay and Arduino
+int IN1 = 13;
+
+//initiliazing connection between the moisture sensor and Arduino
+int Pin1 = A0;
+float sensor_1_val = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(Pin1, INPUT);
+  digitalWrite(IN1, HIGH);
+  delay(500);
+}
+
+void loop() {
+  //More water in soil -> better conductivity -> lower resistance
+  //Less water in soil -> worse conductivity -> higher resistance
+  //---------------------Display--------------------------
+  Serial.print("Moisture level of plant 1: ");
+  sensor_1_val = analogRead(Pin1);
+  Serial.print(sensor_1_val);
+  Serial.print("\n");
+  if (sensor_1_val > 460) {
+    Serial.print("Soil is too dry; needs to be watered");
+  }
+  else {
+    Serial.print("Soil is wet; does not to be watered");
+  }
+  Serial.print("\n");
+  //---------------------Logic--------------------------
+  if(sensor_1_val > 460) {  //if the soil is dry then we open the relay, if wet, close the relay
+    digitalWrite(IN1, LOW);
+  }
+  else {
+    digitalWrite(IN1, HIGH);
+  }
+  delay(5000);
+
+}
